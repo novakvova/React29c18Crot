6b19b621a77454215f63db92033528fd084374ad
novakvova <novakvova@gmail.com>
2019-01-22 09:36:20 +0200

    Add NavigatrionBar

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Route, Switch, Link} from 'react-router-dom';
 import Converter from './Converter';
 import Home from './Home';
+import NavigationBar from './NavigationBar';
 
 
 // import routes from '../routes';
@@ -30,18 +31,7 @@ class App extends React.Component {
         return (
             <div>
 
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <a className="navbar-brand" href="#">Navbar</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div className="navbar-nav">
-                            <Link className="nav-item nav-link active" to="/">Home <span className="sr-only">(current)</span></Link>
-                            <Link className="nav-item nav-link" to="/convert">Converter</Link>
-                        </div>
-                    </div>
-                </nav>
+                <NavigationBar />
 
 
                 <Switch>
